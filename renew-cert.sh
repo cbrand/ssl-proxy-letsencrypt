83be76a7231a2275524f5d40d621b331d14c3f6f
Christoph Brand <christoph.brand@etventure.com>
2017-08-09 18:53:12 +0200

    feature(renew-cert): adjust certificate renewal to not require any kind of keys

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-EMAIL=${cert_email}
-DOMAINS=(${cert_domains})
-
-if [ -z "$DOMAINS" ]; then
-  echo "ERROR: Domain list is empty or unset"
-  exit 1
-fi
-
-if [ -z "$EMAIL" ]; then
-  echo "ERROR: Email is empty string or unset"
-  exit 1
-fi
-
-domain_args=""
-for i in "${DOMAINS[@]}"
-do
-  domain_args="$domain_args -d $i"
-done
-
-if [ -n "${LETSENCRYPT_ENDPOINT+1}" ]; then
-  echo "server = $LETSENCRYPT_ENDPOINT" >> /etc/letsencrypt/cli.ini
-fi
-
 # Renewal of letsencrypt data
 /usr/local/bin/letsencrypt renew \
   --text --agree-tos --webroot \
