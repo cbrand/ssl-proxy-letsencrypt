d2e30ff1ff750cf074ad92bee12a66d6b86d0d78
Christoph Brand <christoph.brand@etventure.com>
2017-08-09 19:05:39 +0200

    feature(renew-cert): updated renew cert script to not require any environment variables

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+filename="/etc/secrets/proxycert"
+origmd5sum=$(md5sum "$filename")
+
 # Renewal of letsencrypt data
 /usr/local/bin/letsencrypt renew \
   --text --agree-tos --webroot \
   --webroot-path /usr/share/nginx/proxy-root
 
-/etc/init.d/nginx reload
+newmd5sum=$(md5sum "$filename")
+
+if [ "$origmd5sum" != "$newmd5sum" ] ; then
+  echo "Reloading nginx"
+  /etc/init.d/nginx reload
+else
+  echo "Nginx is not being reloaded"
+fi
